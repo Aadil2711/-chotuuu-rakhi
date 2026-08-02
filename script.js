c635ef5729c2512f61ff4be7360046d0f5163886
Adil Sheikh <aadil8830@gmail.com>
2026-08-03 00:22:50 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function goToRakhi() {
+    window.location.href = "rakhi.html";
+}
+
+function goToBhaidooj() {
+    window.location.href = "bhaidooj.html";
+}
+
+function goToBirthday() {
+    window.location.href = "birthday.html";
+}
+
+function goToGallery() {
+    window.location.href = "gallery.html";
+}
+
+function goToEnding() {
+    window.location.href = "ending.html";
+}
+function openLetter() {
+
+    document.getElementById("closedLetter").style.display = "none";
+
+    document.getElementById("openedLetter").style.display = "block";
+}
